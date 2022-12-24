a43ef62cb7c01b72d8d1abf934f01710033daf22
Johan Rylander <johan@rylander.cc>
2022-12-24 23:44:11 +0100

    borgnix with flake lock

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11-small";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages.x86_64-linux = {
+      borgnix = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        modules = [
+          ./borgnix.nix
+        ];
+        format = "proxmox-lxc";
+      };
+    };
+  };
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-nix run github:nix-community/nixos-generators -- --configuration borgnix.nix --out-link nixos-borgnix-system-x86_64-linux.tar.xz --format proxmox-lxc
+nix build .#borgnix
